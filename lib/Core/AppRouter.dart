de36c4173eb8cb707c6b421881cc0199dd9115a0
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-07-29 17:43:50 +0000

    add future delay to splash view

--- lib/Core/AppRouter.dart
+++ lib/Core/AppRouter.dart
@@ -4,7 +4,7 @@ import 'package:newsapp/Features/HomeView/Presentation/HomeView.dart';
 import '../Features/SplashView/Presentation/SplashView.dart';
 
 class AppRouter {
-  static String homeView = "homeView";
+  static String homeView = "/homeView";
 
   static final routes = GoRouter(
     routes: <RouteBase>[
--- lib/Features/SplashView/Presentation/Widget/SplashViewBody.dart
+++ lib/Features/SplashView/Presentation/Widget/SplashViewBody.dart
@@ -1,7 +1,21 @@
 import "package:flutter/material.dart";
+import "package:go_router/go_router.dart";
+import "package:newsapp/Core/AppRouter.dart";
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3),
+        () => GoRouter.of(context).push(AppRouter.homeView));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
