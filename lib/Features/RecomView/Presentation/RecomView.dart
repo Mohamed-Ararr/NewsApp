d22d1b8aaca28761c4cc8e27f342eacb34c86f7a
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-07-31 11:25:17 +0000

    add Recommendation screen

---
+++ lib/Features/RecomView/Presentation/RecomView.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+import "Widgets/RecomViewBody.dart";
+
+class RecomView extends StatelessWidget {
+  const RecomView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const RecomViewBody();
+  }
+}
