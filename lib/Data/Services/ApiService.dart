a3cb5cc18004f18f1d1fcd75824308f38447207a
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-08-07 11:46:48 +0000

    api work safely

--- lib/Data/Services/ApiService.dart
+++ lib/Data/Services/ApiService.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 class ApiService {
   final String _apiKey = "56d83944c4504e789d428e9dd0b6c1ac";
@@ -9,6 +10,7 @@ class ApiService {
 
   Future<Map<String, dynamic>> get(String? query) async {
     var response = await dio.get("$_baseUrl?q=$query&apiKey=$_apiKey");
+    debugPrint("${response.data}");
     return response.data;
   }
 }
--- lib/Features/RecomView/Presentation/Widgets/categories.dart
+++ lib/Features/RecomView/Presentation/Widgets/categories.dart
@@ -1,4 +1,6 @@
+import "package:dio/dio.dart";
 import "package:flutter/material.dart";
+import "package:newsapp/Data/Services/ApiService.dart";
 
 import "../../../../Constants.dart";
 import "../../../../Core/AppColors.dart";
@@ -11,20 +13,25 @@ class Categories extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      margin: const EdgeInsets.only(right: 8),
-      alignment: Alignment.center,
-      width: width,
-      decoration: BoxDecoration(
-        borderRadius: kBorderRad10,
-        color: index == 0 ? AppColors.lightBlueColor : AppColors.greyColor,
-      ),
-      child: Text(
-        "hello",
-        style: TextStyle(
-          fontSize: 13,
-          color: index == 0 ? AppColors.whiteColor : Colors.black38,
+    return InkWell(
+      onTap: () async {
+        await ApiService(Dio()).get("dragon-ball");
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        margin: const EdgeInsets.only(right: 8),
+        alignment: Alignment.center,
+        width: width,
+        decoration: BoxDecoration(
+          borderRadius: kBorderRad10,
+          color: index == 0 ? AppColors.lightBlueColor : AppColors.greyColor,
+        ),
+        child: Text(
+          "hello",
+          style: TextStyle(
+            fontSize: 13,
+            color: index == 0 ? AppColors.whiteColor : Colors.black38,
+          ),
         ),
       ),
     );
